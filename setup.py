93ac94e94e668990940d93f7dce729fb5e833046
Abhishek Mishra <abhishekmishra3@gmail.com>
2021-12-25 11:31:47 +0530

    added initial code

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="PicoCmdr",
+    version="0.0.1",
+    packages=["PicoCmdr"],
+    install_requires=[
+        "pysimplegui",
+        "SciFiCmdr"
+        ],
+)
